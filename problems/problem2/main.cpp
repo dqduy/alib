18e82d21fbf148eeb57a0f1e92f6cd933f37e654
qduy88 <qduy88@gmail.com>
2017-10-11 11:24:41 +0700

    Update problem2

---
+++ problems/problem2/main.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+#include<iostream>
+#include "lib/ArrayList.cpp"
+#include "util/ListUtil.cpp"
+
+
+int main()
+{
+    ArrayList a(6);	
+    ListUtil::readList(&a);
+    ListUtil::printList(&a);
+	
+    //Test forr update program for Git over GL
+        	
+    return 0;
+}
