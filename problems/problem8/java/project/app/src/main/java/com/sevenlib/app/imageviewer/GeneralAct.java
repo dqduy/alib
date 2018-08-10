f5000e9661a6aaedd1e913e5c1524d8e77f6e329
qduy88 <qduy88@gmail.com>
2018-08-10 16:52:20 +0700

    Update data storage detection

--- problems/problem8/java/project/app/src/main/java/com/sevenlib/app/imageviewer/GeneralAct.java
+++ problems/problem8/java/project/app/src/main/java/com/sevenlib/app/imageviewer/GeneralAct.java
@@ -8,14 +8,67 @@ package com.sevenlib.app.imageviewer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.view.Window;  
+import java.io.File;
+import android.os.Environment;
 
 public class GeneralAct extends Activity {
-    
+    private final String TAG_NAME = "duy";
     //Pair 1
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Log.i("duy", "onCreate()");
-        super.onCreate(savedInstanceState);        
+        PLog.WriteLog(PLog.MAIN_TAG, "onCReate()");
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        getPathFromDevice();
+    }
+
+    private void getPathFromDevice() {
+
+        File file = this.getFilesDir();
+        PLog.WriteLog(PLog.MAIN_TAG, "=========BEGIN Internal Storage====================" );
+        PLog.WriteLog(PLog.MAIN_TAG, "getName(): " + file.getName());
+        PLog.WriteLog(PLog.MAIN_TAG, "getPath():         " + file.getPath());
+        PLog.WriteLog(PLog.MAIN_TAG, "getAbsolutePath(): " + file.getAbsolutePath());
+        PLog.WriteLog(PLog.MAIN_TAG, "=========END Internal Storage====================" );
+
+        file = this.getExternalFilesDir(null);
+        PLog.WriteLog(PLog.MAIN_TAG, "=========BEGIN Private - External Storage====================" );
+        PLog.WriteLog(PLog.MAIN_TAG, "getName(): " + file.getName());
+        PLog.WriteLog(PLog.MAIN_TAG, "getPath():         " + file.getPath());
+        PLog.WriteLog(PLog.MAIN_TAG, "getAbsolutePath(): " + file.getAbsolutePath());
+        PLog.WriteLog(PLog.MAIN_TAG, "=========END Private - External Storage=====================" );
+
+        file = Environment.getExternalStoragePublicDirectory (Environment.DIRECTORY_PICTURES);
+        PLog.WriteLog(PLog.MAIN_TAG, "=========BEGIN Public - External Storage====================" );
+        PLog.WriteLog(PLog.MAIN_TAG, "getName(): " + file.getName());
+        PLog.WriteLog(PLog.MAIN_TAG, "getPath():         " + file.getPath());
+        PLog.WriteLog(PLog.MAIN_TAG, "getAbsolutePath(): " + file.getAbsolutePath());
+        PLog.WriteLog(PLog.MAIN_TAG, "=========END Public - External Storage=====================" );
+
+        file = Environment.getExternalStorageDirectory ();
+        PLog.WriteLog(PLog.MAIN_TAG, "=========BEGIN Primary - External Storage====================" );
+        PLog.WriteLog(PLog.MAIN_TAG, "getName(): " + file.getName());
+        PLog.WriteLog(PLog.MAIN_TAG, "getPath():         " + file.getPath());
+        PLog.WriteLog(PLog.MAIN_TAG, "getAbsolutePath(): " + file.getAbsolutePath());
+        PLog.WriteLog(PLog.MAIN_TAG, "=========END Primary - External Storage=====================" );
+
+        File[] list = this.getExternalMediaDirs();
+        int length  = list.length;
+        PLog.WriteLog(PLog.MAIN_TAG, "List sdcard: " + length);
+
+        for(int index = 0; index < length; ++index) {
+            file = list[index];
+            PLog.WriteLog(PLog.MAIN_TAG, "=========BEGIN External Storage==================== " + index);
+            PLog.WriteLog(PLog.MAIN_TAG, "getName(): " + file.getName());
+            PLog.WriteLog(PLog.MAIN_TAG, "getPath():         " + file.getPath());
+            PLog.WriteLog(PLog.MAIN_TAG, "getAbsolutePath(): " + file.getAbsolutePath());
+            PLog.WriteLog(PLog.MAIN_TAG, "=========END   External Storage=====================" );
+        }
+
+
     }
 
     @Override
