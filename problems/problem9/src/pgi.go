a569c0ae087c35ffb38248601a698a8b570ec1ab
Duy Quoc <qduy88@gmail.com>
2018-09-19 22:12:54 +0700

    Update pgi app from golang

--- problems/problem9/src/pgi.go
+++ problems/problem9/src/pgi.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"container/list"
 	"fmt"
-	//"io/ioutil"
 	"os"
 	"sort"
 	"strconv"
@@ -64,11 +63,15 @@ func (this ByPoint) Swap(i, j int) {
 	this[i], this[j] = this[j], this[i]
 }
 
+/////////////////////////////////////////////////////////////
+
 //Global data
-var listOfTeam *list.List
-var listOfPointDistribution *list.List
-var listOfMatches *list.List
-var resultList []ResultItem
+var listOfTeam *list.List = list.New()
+var listOfPointDistribution *list.List = list.New()
+var listOfMatchesTpp *list.List = list.New()
+var listOfMatchesFpp *list.List = list.New()
+var resultListTpp []ResultItem
+var resultListFpp []ResultItem
 
 const dbName = "pgi.txt"
 
@@ -82,37 +85,37 @@ func CreateFakeData() {
 	team2 := Team{2, "Welcome to South Georgo", "European Qualifier"}
 	team3 := Team{9, "Gen.G Gold", "Korean Qualifier"}
 
-	//listOfTeam.PushBack(team1)
-	//listOfTeam.PushBack(team2)
-	//listOfTeam.PushBack(team3)
+	listOfTeam.PushBack(team1)
+	listOfTeam.PushBack(team2)
+	listOfTeam.PushBack(team3)
 
 	//2. Create point distributions
 	listOfPointDistribution = list.New()
-	// listOfPointDistribution.PushBack(PointDistribution{0, 15})
-	// listOfPointDistribution.PushBack(PointDistribution{1, 500})
-	// listOfPointDistribution.PushBack(PointDistribution{2, 410})
-	// listOfPointDistribution.PushBack(PointDistribution{3, 345})
-	// listOfPointDistribution.PushBack(PointDistribution{4, 295})
-	// listOfPointDistribution.PushBack(PointDistribution{5, 250})
-	// listOfPointDistribution.PushBack(PointDistribution{6, 210})
-	// listOfPointDistribution.PushBack(PointDistribution{7, 175})
-	// listOfPointDistribution.PushBack(PointDistribution{8, 145})
-	// listOfPointDistribution.PushBack(PointDistribution{9, 120})
-	// listOfPointDistribution.PushBack(PointDistribution{10, 100})
-	// listOfPointDistribution.PushBack(PointDistribution{11, 80})
-	// listOfPointDistribution.PushBack(PointDistribution{12, 65})
-	// listOfPointDistribution.PushBack(PointDistribution{13, 50})
-	// listOfPointDistribution.PushBack(PointDistribution{14, 40})
-	// listOfPointDistribution.PushBack(PointDistribution{15, 30})
-	// listOfPointDistribution.PushBack(PointDistribution{16, 20})
-	// listOfPointDistribution.PushBack(PointDistribution{17, 15})
-	// listOfPointDistribution.PushBack(PointDistribution{18, 10})
-	// listOfPointDistribution.PushBack(PointDistribution{19, 5})
-	// listOfPointDistribution.PushBack(PointDistribution{20, 0})
+	listOfPointDistribution.PushBack(PointDistribution{0, 15})
+	listOfPointDistribution.PushBack(PointDistribution{1, 500})
+	listOfPointDistribution.PushBack(PointDistribution{2, 410})
+	listOfPointDistribution.PushBack(PointDistribution{3, 345})
+	listOfPointDistribution.PushBack(PointDistribution{4, 295})
+	listOfPointDistribution.PushBack(PointDistribution{5, 250})
+	listOfPointDistribution.PushBack(PointDistribution{6, 210})
+	listOfPointDistribution.PushBack(PointDistribution{7, 175})
+	listOfPointDistribution.PushBack(PointDistribution{8, 145})
+	listOfPointDistribution.PushBack(PointDistribution{9, 120})
+	listOfPointDistribution.PushBack(PointDistribution{10, 100})
+	listOfPointDistribution.PushBack(PointDistribution{11, 80})
+	listOfPointDistribution.PushBack(PointDistribution{12, 65})
+	listOfPointDistribution.PushBack(PointDistribution{13, 50})
+	listOfPointDistribution.PushBack(PointDistribution{14, 40})
+	listOfPointDistribution.PushBack(PointDistribution{15, 30})
+	listOfPointDistribution.PushBack(PointDistribution{16, 20})
+	listOfPointDistribution.PushBack(PointDistribution{17, 15})
+	listOfPointDistribution.PushBack(PointDistribution{18, 10})
+	listOfPointDistribution.PushBack(PointDistribution{19, 5})
+	listOfPointDistribution.PushBack(PointDistribution{20, 0})
 	//fmt.Println(*listOfPointDistribution)
 
 	//3. Create matches & match details
-	listOfMatches = list.New()
+	listOfMatchesTpp = list.New()
 
 	//Round 1
 	matchdetail1 := MatchDetails{team1, PointDistribution{3, 345}, 7, 0}
@@ -155,25 +158,9 @@ func CreateFakeData() {
 	match3.listOfMatchDetails.PushBack(matchdetail9)
 
 	//Add matches
-	listOfMatches.PushBack(match1)
-	listOfMatches.PushBack(match2)
-	listOfMatches.PushBack(match3)
-}
-
-func ParseTeams() {
-
-}
-
-func ParsePointDistributions() {
-
-}
-
-func ParseTPP() {
-
-}
-
-func ParseFPP() {
-
+	listOfMatchesTpp.PushBack(match1)
+	listOfMatchesTpp.PushBack(match2)
+	listOfMatchesTpp.PushBack(match3)
 }
 
 func MakeTeam(id int, name string, regional string) Team {
@@ -184,9 +171,32 @@ func MakePointDistribution(placement int, point int) PointDistribution {
 	return PointDistribution{placement, point}
 }
 
+func SearchTeam(id int) Team {
+	for index := listOfTeam.Front(); index != nil; index = index.Next() {
+		if id == index.Value.(Team).id {
+			return index.Value.(Team)
+		}
+	}
+
+	return Team{0, "", ""}
+}
+
+func SearchPointDistribution(placement int) PointDistribution {
+	for index := listOfPointDistribution.Front(); index != nil; index = index.Next() {
+		if placement == index.Value.(PointDistribution).placement {
+			return index.Value.(PointDistribution)
+		}
+	}
+
+	return PointDistribution{0, 15}
+}
+
+/////////////////////////////////////////////////////
+
 //1. Load database
 func LoadData() {
-	CreateFakeData()
+	//For testing
+	//CreateFakeData()
 
 	file, err := os.Open(dbName)
 	if err != nil {
@@ -201,22 +211,16 @@ func LoadData() {
 
 	for scanner.Scan() {
 		str = strings.TrimSpace(scanner.Text())
-		//var lineLen = len(str)
 
 		if str == "team" {
 			section = 1
-			fmt.Printf("%s - len: %d\n", str, len(str))
 		} else if str == "points distribution" {
 			section = 2
-			fmt.Printf("%s - len: %d\n", str, len(str))
 		} else if str == "tpp" {
 			section = 3
-			fmt.Printf("%s - len: %d\n", str, len(str))
 		} else if str == "fpp" {
 			section = 4
-			fmt.Printf("%s - len: %d\n", str, len(str))
 		} else {
-			//fmt.Printf("other text - len: %d\n", len(str))
 			if len(str) > 0 {
 				switch section {
 				case 1:
@@ -230,6 +234,7 @@ func LoadData() {
 					listOfTeam.PushBack(MakeTeam(id,
 						strings.TrimSpace(tokens[1]),
 						strings.TrimSpace(tokens[2])))
+
 				case 2:
 					var tokens = strings.Split(str, ",")
 					placement, err := strconv.Atoi(strings.TrimSpace(tokens[0]))
@@ -240,44 +245,82 @@ func LoadData() {
 					}
 
 					listOfPointDistribution.PushBack(MakePointDistribution(placement, point))
-					//fmt.Printf("%s - len: %d\n", str, len(str))
+
 				case 3:
-					//fmt.Printf("%s - len: %d\n", str, len(str))
-					//9, 8, 1,1, 6, 6,2, 18, 2,11, 5, 6,3, 12, 3,10, 14, 3,17, 15, 2,5, 1, 12,18, 7, 4,7, 13, 6,19, 4, 4,15, 17, 1,12, 3, 9,13, 2, 4,6, 9, 3,8, 11, 2,20, 10, 4,14, 20, 0,16, 19, 0,4, 16, 1
 					var tokens = strings.Split(str, ",")
 					var tokensLen = len(tokens)
+					//fmt.Println(tokensLen)
+
+					id, err := strconv.Atoi(strings.TrimSpace(tokens[0]))
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					match := Match{id, tokens[1], tokens[2], list.New()}
+
+					for i := 0; i < (tokensLen-3)/3; i++ {
+						teamid, err := strconv.Atoi(strings.TrimSpace(tokens[3+(i*3)]))
+						placement, err := strconv.Atoi(strings.TrimSpace(tokens[3+(i*3)+1]))
+						kill, err := strconv.Atoi(strings.TrimSpace(tokens[3+(i*3)+2]))
+						if err != nil {
+							fmt.Println(err)
+						}
+						matchdetail := MatchDetails{SearchTeam(teamid), SearchPointDistribution(placement), kill, 0}
+						matchdetail.caculatePoint()
+
+						match.listOfMatchDetails.PushBack(matchdetail)
+					}
+
+					listOfMatchesTpp.PushBack(match)
 
 				case 4:
-					//fmt.Printf("%s - len: %d\n", str, len(str))
+					var tokens = strings.Split(str, ",")
+					var tokensLen = len(tokens)
+					//fmt.Println(tokensLen)
+
+					id, err := strconv.Atoi(strings.TrimSpace(tokens[0]))
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					match := Match{id, tokens[1], tokens[2], list.New()}
+
+					for i := 0; i < (tokensLen-3)/3; i++ {
+						teamid, err := strconv.Atoi(strings.TrimSpace(tokens[3+(i*3)]))
+						placement, err := strconv.Atoi(strings.TrimSpace(tokens[3+(i*3)+1]))
+						kill, err := strconv.Atoi(strings.TrimSpace(tokens[3+(i*3)+2]))
+						if err != nil {
+							fmt.Println(err)
+						}
+						matchdetail := MatchDetails{SearchTeam(teamid), SearchPointDistribution(placement), kill, 0}
+						matchdetail.caculatePoint()
+
+						match.listOfMatchDetails.PushBack(matchdetail)
+					}
+
+					listOfMatchesFpp.PushBack(match)
+
 				default:
 					fmt.Printf("Unknown section")
 				}
 			} else {
-				fmt.Println("We have a blank line....")
+				//fmt.Println("We have a blank line....")
 			}
 		}
-
-		//fmt.Printf("%s\n", str)
-
 	}
-
-	// var s = "jkk   jk   kj   "
-	// fmt.Printf("%q", s[0])
-	// fmt.Println("len: ", len(strings.TrimSpace(s)), " - ", strings.TrimSpace(s))
-
 }
 
 //2. Calculate table
 func CalculatePGI() {
-	resultList = make([]ResultItem, 0)
+	resultListTpp = make([]ResultItem, 0)
 
-	//Filter team result
+	//Filter team result TPP
 	for item := listOfTeam.Front(); item != nil; item = item.Next() {
 		var data = item.Value.(Team)
 		resultItem := ResultItem{data, list.New(), 0}
 
 		//Each team we get all match details of each
-		for inner := listOfMatches.Front(); inner != nil; inner = inner.Next() {
+		for inner := listOfMatchesTpp.Front(); inner != nil; inner = inner.Next() {
 			var list = inner.Value.(Match).listOfMatchDetails
 			//Add detail of specific match
 			for i := list.Front(); i != nil; i = i.Next() {
@@ -288,14 +331,33 @@ func CalculatePGI() {
 				}
 			}
 		}
-		resultList = append(resultList, resultItem)
+		resultListTpp = append(resultListTpp, resultItem)
+	}
+
+	for item := listOfTeam.Front(); item != nil; item = item.Next() {
+		var data = item.Value.(Team)
+		resultItem := ResultItem{data, list.New(), 0}
+
+		//Each team we get all match details of each
+		for inner := listOfMatchesFpp.Front(); inner != nil; inner = inner.Next() {
+			var list = inner.Value.(Match).listOfMatchDetails
+			//Add detail of specific match
+			for i := list.Front(); i != nil; i = i.Next() {
+				var detail = i.Value.(MatchDetails)
+				if detail.team.id == data.id {
+					resultItem.listOfMatchDetails.PushBack(detail)
+					resultItem.totalPoint += detail.detail.point + detail.kill*15
+				}
+			}
+		}
+		resultListFpp = append(resultListFpp, resultItem)
 	}
 
 	//Sort result list
 	//1. Point
 	//2. if two point's team is equal, we compare kill
-	sort.Sort(ByPoint(resultList))
-
+	sort.Sort(ByPoint(resultListTpp))
+	sort.Sort(ByPoint(resultListFpp))
 }
 
 //3. Display section
@@ -318,17 +380,19 @@ func DisplayMatch(match Match) string {
 	return result
 }
 
-func DisplayResultItems() string {
+func DisplayResultItems(list []ResultItem) string {
 	var result = ""
-
-	for _, item := range resultList {
-		result += item.team.name + "\t"
+	var start = 1
+	for _, item := range list {
+		result += strconv.Itoa(start) + ". " + item.team.name + "\t\t"
 		for inner := item.listOfMatchDetails.Front(); inner != nil; inner = inner.Next() {
 			var data = inner.Value.(MatchDetails)
 			result += "[" + strconv.Itoa(data.detail.placement) + ", " + strconv.Itoa(data.kill) + "]" + " - "
 
 		}
 		result += strconv.Itoa(item.totalPoint) + "\n"
+
+		start++
 	}
 	return result
 }
@@ -357,10 +421,10 @@ func DisplayPointDistribution() {
 }
 
 func DisplayMatches() {
-	if listOfMatches != nil {
-		fmt.Println("Total matches: ", listOfMatches.Len())
+	if listOfMatchesTpp != nil {
+		fmt.Println("Total matches: ", listOfMatchesTpp.Len())
 
-		for index := listOfMatches.Front(); index != nil; index = index.Next() {
+		for index := listOfMatchesTpp.Front(); index != nil; index = index.Next() {
 			fmt.Print(DisplayMatch(index.Value.(Match)))
 			fmt.Print("\n")
 		}
@@ -369,16 +433,21 @@ func DisplayMatches() {
 
 func DisplayAll() {
 	//1. Print teams
-	DisplayAllTeams()
+	//DisplayAllTeams()
 
 	//2. Print point distributions
-	DisplayPointDistribution()
+	//DisplayPointDistribution()
 
 	//3. Print matches
 	//DisplayMatches()
 
 	//4. Print result items
-	fmt.Println(DisplayResultItems())
+	fmt.Println("PUBG Global Invitational 2018")
+	fmt.Println("=============================\n")
+	fmt.Println("TPP:")
+	fmt.Println(DisplayResultItems(resultListTpp))
+	fmt.Println("FPP:")
+	fmt.Println(DisplayResultItems(resultListFpp))
 }
 
 func main() {
